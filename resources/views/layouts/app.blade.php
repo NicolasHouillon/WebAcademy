72b5fdc6c446f67a339dcf528bdde697cde0a8d4
Remy Karl <karl_remy@ens.univ-artois.fr>
2020-09-17 15:14:16 +0200

    resolution bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,13 +134,13 @@
                         </div>
                     @endif
                 @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <li class="nav-item">
+                        <a id="navbarDropdown" class="nav-link" href="tableauBord">
                             {{ Auth::user()->name }}
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                        <div class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
--- resources/views/tableauBord.blade.php
+++ resources/views/tableauBord.blade.php
@@ -3,19 +3,19 @@
 @section('content')
 
 <div class="container-md">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="carte">
-                <div class="photo">
-                    <img src="image/Karlphoto.png" class="rounded-circle">
-                </div>
+    <div class="col-sm-4">
+        <div class="card" style="width:400px">
+            <img class="card-img-top" src="image/Karlphoto.png" alt="Card image" style="width:50%">
+            <div class="card-body">
+                <h4 class="card-title">{{ Auth::user()->name }}</h4>
+                <p class="card-text"></p>
+                <a href="#" class="btn btn-primary">See Profile</a>
             </div>
-            <div class="message">
-
-            </div>
-            <div class="message">
-
-            </div>
+        </div>
+    </div>
+    <div class="col-sm-8">
+        <div class="message">
+            Message
         </div>
     </div>
 </div>
