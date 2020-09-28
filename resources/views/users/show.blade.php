bfe99b68cf60cd13d493d769af55f0cbb5dfda13
Remy Karl <karl_remy@ens.univ-artois.fr>
2020-09-28 12:12:56 +0200

    juste pour pull

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    @if($error)
+    @if(isset($error))
         <h1>{{ $error }}</h1>
     @else
         <div class="container">
@@ -11,17 +11,19 @@
                     <img class="card-img-top" src="image/Karlphoto.png" alt="Card image" style="width:50%">
                     <div class="card-body">
                         <h4 class="card-title">{{ $user->name }}</h4>
-                        <p class="card-text"></p>
-                        <a href="#" class="btn btn-primary">See Profile</a>
+                        <p class="card-text">{{ $user-> }}</p>
+                        <a href="#" class="btn btn-primary">Modifier le profil</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-8">
-                <div class="card">
-                    Message
-                    <br><br><br><br><br><br><br><br><br><br><br><br><br>
+            @if(Auth::user()->name == $user->name)
+                <div class="col-sm-8">
+                    <div class="card">
+                        Message
+                        <br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    </div>
                 </div>
-            </div>
+            @endauth
         </div>
     @endif
 
