e19283073c71bfbf8c10ed91007dc3a8a2a21dfb
Remy Karl <karl_remy@ens.univ-artois.fr>
2020-09-28 15:38:18 +0200

    page user v1

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,9 +8,9 @@
         <div class="container">
             <div class="col-sm-4">
                 <div class="card" style="width:400px">
-                    <img class="card-img-top" src="image/Karlphoto.png" alt="Card image" style="width:50%">
+                    <img class="card-img-top" src="{{ $user->avatar }}" alt="Card image" style="width:50%">
                     <div class="card-body">
-                        <h4 class="card-title">{{ $user->firstname }}</h4>
+                        <p class="card-text">{{ $user->firstname }}</p>
                         <p class="card-text">{{ $user->lastname }}</p>
                         <p class="card-text">{{ $user->email }}</p>
                         <a href="#" class="btn btn-primary">Modifier le profil</a>
@@ -21,7 +21,6 @@
                 <div class="col-sm-8">
                     <div class="card">
                         Message
-                        <br><br><br><br><br><br><br><br><br><br><br><br><br>
                     </div>
                 </div>
             @endauth
