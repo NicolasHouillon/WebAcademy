070d29bdf94a7b5900d29bfc89db7b5df6314480
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2020-10-22 14:13:39 +0200

    remplissage footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,48 @@
 <footer class="footer">
-    <p>Ceci est le footer.</p>
+    <div class="message">
+        <a href="{{ route('contact') }}">
+            Envoyer un message
+        </a>
+    </div>
+
+    <ul class="liens">
+        <p><b>Liens utiles :</b></p>
+        <li>
+            <a href="{{ route('index') }}">Accueil</a>
+        </li>
+        <li>
+            <a href="{{ route('annuaire') }}">Annuaire</a>
+        </li>
+        @auth()
+        <li>
+            <a href="{{route('user_profile', ['name' => Auth::user()->slugFullName()])}}">Profil</a>
+        </li>
+        @endauth
+        <br><br>
+        <li>
+            <a href="{{ route('messages') }}">Messages</a>
+        </li>
+        <li>
+            <a href="#">Recrutement</a>
+        </li>
+    </ul>
+
+    <ul class="createur">
+        <p><b>Créateurs du site :</b></p>
+        <li>
+            CERNUTA Valentin
+        </li>
+        <li>
+            BONNAIRE Eric
+        </li>
+        <br><br>
+        <li>
+            REMY Karl
+        </li>
+        <li>
+            HOUILLON Nicolas
+        </li>
+    </ul>
+
 </footer>
 
